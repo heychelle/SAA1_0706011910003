2edad867ad4abd7a9b1466913e5dcb0fa62d10d1
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2020-10-17 18:29:46 +0700

    lasttttt

--- app/src/main/java/com/uc/saa1_0706011910003/StarterActivity.java
+++ app/src/main/java/com/uc/saa1_0706011910003/StarterActivity.java
@@ -9,20 +9,31 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class StarterActivity extends AppCompatActivity {
 
     ImageView button_add_student, button_add_lecturer, button_add_course, button_login_student;
     Intent intent;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starter);
 
+        firebaseAuth = FirebaseAuth.getInstance();
         button_add_student = findViewById(R.id.button_add_student);
         button_add_lecturer = findViewById(R.id.button_add_lecturer);
         button_add_course = findViewById(R.id.button_add_course);
         button_login_student = findViewById(R.id.button_login_student);
+
+        if(firebaseAuth.getCurrentUser() != null){
+            Toast.makeText(StarterActivity.this,"Welcome Back", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(StarterActivity.this, MainFragment.class);
+            startActivity(intent);
+            finish();
+        }
 
         button_add_student.setOnClickListener(new View.OnClickListener() {
             @Override
